f72e6025ad76c1b964f3d9f630e91c8e54357c0c
KIMJONGHO123 <ho098701@naver.com>
2025-05-07 02:00:02 +0900

    V0.21(관리자 페이지 개발 중 )

---
+++ admin_page/src/pages/ProductPage.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function ProductPage() {
+  return (
+    <Typography variant="h4" fontWeight="bold">
+      상품 관리 페이지입니다
+    </Typography>
+  );
+}
