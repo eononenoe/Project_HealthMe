7f25e0090df677fc0bbf227cb595d90169aae75e
HTH-0 <xogud1793@naver.com>
2025-05-07 15:48:51 +0900

    dd

---
+++ 스프링부트빼내서작업/healthme/src/main/java/com/example/healthme/HealthmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthmeApplication.class, args);
+	}
+
+}
