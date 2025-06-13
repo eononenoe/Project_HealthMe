70e06c1614ebe037b2093cbfbf3a3b8375a1132b
smj1249 <smj1249@gmail.com>
2025-06-13 10:02:54 +0900

    2025-06-13

--- healthme/frontend/src/pages/Complete/complete.jsx
+++ healthme/frontend/src/pages/Complete/complete.jsx
@@ -5,7 +5,7 @@ import "static/css/pages/complete.css";
 export default function Complete() {
   return (
     <div className="order-complete-container">
-      <h1>🎉 주문이 완료되었습니다!</h1>
+      <h1>주문이 완료되었습니다!</h1>
       <p>주문해 주셔서 감사합니다.</p>
       <p>주문 내역은 마이페이지에서 확인하실 수 있습니다.</p>
       <Link to="/" className="go-home-button">
